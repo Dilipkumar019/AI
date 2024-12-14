39a7b74857bef1e1ac094738c515a6a1419c343f
Dilipkumar019 <dilipkumar.kasina@sjsu.edu>
2024-12-14 01:44:13 -0800

    q1

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from '@clerk/nextjs';
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Fintrk-AI",
+  description: "SmartPocket",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+    publishabkey = {process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
